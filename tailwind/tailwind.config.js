81f1eba3026638c1265cf6850fc6972beeb74ed7
Ana Laf <zanlamar@gmail.com>
2025-06-04 17:59:03 +0200

    menumedio H+D ok

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
+
+  safelist: [
+    'border-b-4',
+    'border-red-500',
+    'hover:border-blue-500',
+    'hover:text-black',
+  ],
+  
   theme: {
     extend: {},
   },
